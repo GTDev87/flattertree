0897ff9fad382beb31d552bd0e6467551305bc0f
Gregory Thompson <greg@mashwork.com>
2013-07-26 19:46:50 -0400

    Updated version of flattertree

--- test/flattertree.js
+++ test/flattertree.js
@@ -1,18 +1,18 @@
-var flatree = require('../lib/flattertree');
+var flattertree = require('../lib/flattertree');
 
-flatree.connect("mongodb://localhost/generic", function(){
-	flatree.insertNode("directory", null, null, [], {hello: "world", object: {object: "stuff"}, arr: ["hello", "govener"], mormon: [{my: "name"},{is: "elder"},{price: "cunningham"}], good_data: [{id: 23, hello: "hello"}, {id: 34, hello: "world"}, {id: 45, hello: "neighbor"}]}, function(){
-		flatree.insertNode("more_stuff", ",directory,", null, [], {I: "like", data: "sir"}, function(){
+flattertree.connect("mongodb://localhost/generic", function(){
+	flattertree.insertNode("directory", null, null, [], {hello: "world", object: {object: "stuff"}, arr: ["hello", "govener"], mormon: [{my: "name"},{is: "elder"},{price: "cunningham"}], good_data: [{id: 23, hello: "hello"}, {id: 34, hello: "world"}, {id: 45, hello: "neighbor"}]}, function(){
+		flattertree.insertNode("more_stuff", ",directory,", null, [], {I: "like", data: "sir"}, function(){
 			console.log("about to find");
-			flatree.fullNodeData(",directory,", function(data){
+			flattertree.fullNodeData(",directory,", function(data){
 				console.log("data = %j", data);
-				flatree.findNode(",directory,", function(node){
+				flattertree.findNode(",directory,", function(node){
 					console.log("node = %j", node);
-					flatree.findFullNode(",directory,", function(data){
+					flattertree.findFullNode(",directory,", function(data){
 						console.log("full data = %j", data);
-						flatree.deleteNode(",directory,#arr,", function(){
-							flatree.updateNode(",directory,", null, [], {i: "feel", great: "now"}, function(){
-								flatree.findNode(",directory,", function(node){
+						flattertree.deleteNode(",directory,#arr,", function(){
+							flattertree.updateNode(",directory,", null, [], {i: "feel", great: "now"}, function(){
+								flattertree.findNode(",directory,", function(node){
 									console.log("node = %j", node);
 									console.log("success");
 								}, 
