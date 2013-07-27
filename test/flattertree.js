3631bb6d59d1a2c67f0f94cfe484eaf2c830844a
Gregory Thompson <greg@mashwork.com>
2013-07-27 17:53:54 -0400

    Made project jslint compliant

--- test/flattertree.js
+++ test/flattertree.js
@@ -1,33 +1,46 @@
+/*jslint nomen: true */
+'use strict';
+
 var flattertree = require('../lib/flattertree');
 
-flattertree.connect("mongodb://localhost/generic", function(){
-	flattertree.insertNode("directory", null, null, [], {hello: "world", object: {object: "stuff"}, arr: ["hello", "govener"], mormon: [{my: "name"},{is: "elder"},{price: "cunningham"}], good_data: [{id: 23, hello: "hello"}, {id: 34, hello: "world"}, {id: 45, hello: "neighbor"}]}, function(){
-		flattertree.insertNode("more_stuff", ",directory,", null, [], {I: "like", data: "sir"}, function(){
-			console.log("about to find");
-			flattertree.fullNodeData(",directory,", function(data){
-				console.log("data = %j", data);
-				flattertree.findNode(",directory,", function(node){
-					console.log("node = %j", node);
-					flattertree.findFullNode(",directory,", function(data){
-						console.log("full data = %j", data);
-						flattertree.deleteNode(",directory,#arr,", function(){
-							flattertree.updateNode(",directory,", null, [], {i: "feel", great: "now"}, function(){
-								flattertree.findNode(",directory,", function(node){
-									console.log("node = %j", node);
-									console.log("success");
-								}, 
-								function(){
-									console.log("failed at update");
+flattertree.connect("mongodb://localhost/generic", function (dataTree) {
+	dataTree.insertNode(
+		"directory",
+		null,
+		null,
+		[],
+		{hello: "world", object: {object: "stuff"}, arr: ["hello", "govener"], mormon: [{my: "name"}, {is: "elder"}, {price: "cunningham"}], good_data: [{id: 23, hello: "hello"}, {id: 34, hello: "world"}, {id: 45, hello: "neighbor"}]},
+		function () {
+			dataTree.insertNode("more_stuff", ",directory,", null, [], {I: "like", data: "sir"}, function () {
+				console.log("about to find");
+				dataTree.fullNodeData(",directory,", function (data) {
+					console.log("data = %j", data);
+					dataTree.findNode(",directory,", function (node) {
+						console.log("node = %j", node);
+						dataTree.findFullNode(",directory,", function (data) {
+							console.log("full data = %j", data);
+							dataTree.deleteNode(",directory,#arr,", function () {
+								dataTree.updateNode(",directory,", null, [], {i: "feel", great: "now"}, function () {
+									dataTree.findNode(
+										",directory,",
+										function (node) {
+											console.log("node = %j", node);
+											console.log("success");
+										},
+										function () {
+											console.log("failed at update");
+										}
+									);
 								});
 							});
 						});
 					});
 				});
 			});
-		});
-	}, 
-	function(err){
-		console.log("failure");
-		throw err;
-	});
+		},
+		function (err) {
+			console.log("failure");
+			throw err;
+		}
+	);
 });
